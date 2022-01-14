8ec0060ba4fe86edf56def00b471df799280d44f
mgarnick1 <markgman008@gmail.com>
2022-01-13 18:17:52 -0700

    added the dotenv package from deno and switched to the env get methods

--- db/database.js
+++ db/database.js
@@ -1,21 +1,23 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+
 
 class Database {
-    constructor() {
-        this.connect();
-    }
+  constructor() {
+    this.connect();
+  }
 
-    async connect() {
-        this.client = new Client({
-            user: 'postgres',
-            database: 'demo_database',
-            hostname: 'localhost',
-            password: 'postgress55',
-            port: 5432
-        })
+  async connect() {
+    this.client = new Client({
+      user: Deno.env.get("DB_USER"),
+      database: Deno.env.get("DBNAME"),
+      hostname: Deno.env.get("DB_HOST"),
+      password: Deno.env.get("DB_PASS"),
+      port: 5432,
+    });
 
-        await this.client.connect();
-    }
+    await this.client.connect();
+  }
 }
 
-export default new Database().client
+export default new Database().client;
